42a9a78457cff6e10fff5ee0cab755908e340a10
Amine Bouhaddi <aminebouhaddi@Amines-MacBook-Pro.local>
2024-03-08 14:03:25 -0800

    add categories

--- app/Domain/Posts/Models/Category.php
+++ app/Domain/Posts/Models/Category.php
@@ -5,14 +5,14 @@ namespace App\Domain\Posts\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Category extends Model
 {
     use HasFactory;
 
     public $guarded = [];
 
-    public function category()
+    public function posts()
     {
-        return $this->belongsTo(Category::class);
+        return $this->hasMany(Post::class);
     }
 }
--- database/migrations/2024_03_03_052840_create_categories_table.php
+++ database/migrations/2024_03_03_052840_create_categories_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('slug')->unique()->nullable();
-            $table->text('content');
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
     }
 };
